8de5154485495fc3e055ec36227818dfc3515e26
Gleb Kuzmin <kuzmin@owlcat.games>
2025-02-24 22:18:53 +0100

    something

--- Assets/Game/Code/TestObject.cs
+++ Assets/Game/Code/TestObject.cs
@@ -1,18 +1,22 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using UnityEditor;
 using UnityEngine;
 
 namespace Sadalmalik.TheGrowth
 {
+    [Serializable]
     public class TestObjectData
     {
-        public AssetReference<CardConfig> config;
+        public AssetReference<CardConfig> config1;
+        public AssetReference<CardConfig> config2;
     }
 
     [ExecuteInEditMode]
     public class TestObject : MonoBehaviour
     {
-        public CardConfig config;
+        public CardConfig config1;
+        public CardConfig config2;
 
         public bool test;
 
@@ -22,14 +26,17 @@ namespace Sadalmalik.TheGrowth
             test = false;
             
             var data = new TestObjectData();
-            data.config.Value = config;
+            data.config1.Value = config1;
+            data.config2.Value = config2;
             var json = JsonConvert.SerializeObject(data);
             Debug.Log(json);
 
             var loadedData= JsonConvert.DeserializeObject<TestObjectData>(json);
             
-            Debug.Log(loadedData.config);
-            Debug.Log(loadedData.config.Value);
+            Debug.Log(loadedData.config1);
+            Debug.Log(loadedData.config1.Value);
+            Debug.Log(loadedData.config2);
+            Debug.Log(loadedData.config2.Value);
         }
     }
 }
