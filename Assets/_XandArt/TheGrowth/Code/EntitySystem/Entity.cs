9ceb8c181ccaf77956cc9a6c27c57410075a7240
Gleb Kuzmin <kuzmin@owlcat.games>
2025-04-26 20:01:05 +0200

    Reorder folders

---
+++ Assets/_XandArt/TheGrowth/Code/EntitySystem/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace XandArt.TheGrowth
+{
+    [Serializable]
+    public class Entity
+    {
+        [JsonIgnore]
+        public EntityView View;
+        
+        public virtual void OnPreSave() {}
+        public virtual void OnPostLoad() {}
+    }
+}
---
+++ Assets/_XandArt/TheGrowth/Code/EntitySystem/PersistenceManager.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace XandArt.TheGrowth
+{
+    public class PersistenceManager
+    {
+        private static readonly JsonSerializerSettings SerializationSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            TypeNameHandling = TypeNameHandling.All
+        };
+
+        private const string SaveFolder = "save";
+
+        public World Load(string saveId)
+        {
+            var file = Path.Combine(Application.persistentDataPath, SaveFolder, saveId);
+            
+            var json = File.ReadAllText(file);
+            var world = JsonConvert.DeserializeObject<World>(json, SerializationSettings);
+            world.OnPostLoad();
+            return world;
+        }
+        
+        public void Save(string saveId, World world)
+        {
+            var file = Path.Combine(Application.persistentDataPath, SaveFolder, saveId);
+            
+            world.OnPreSave();
+            var json = JsonConvert.SerializeObject(world, SerializationSettings);
+            File.WriteAllText(file, json);
+        }
+    }
+}
