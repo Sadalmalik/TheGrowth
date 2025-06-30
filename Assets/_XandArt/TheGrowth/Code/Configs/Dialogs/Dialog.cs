e41bf4a5a8213f0e3b3c5719703343b56a9948e9
Gleb Kuzmin <kuzmin@owlcat.games>
2025-06-30 20:10:55 +0200

    Пример конфига диалогов

---
+++ Assets/_XandArt/TheGrowth/Code/Configs/Dialogs/Dialog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using XandArt.Architecture;
+
+namespace XandArt.TheGrowth
+{
+    public abstract class DialogNode
+    {
+        public string next;
+    }
+
+    public class ReplicaNode : DialogNode
+    {
+        public string text;
+    }
+
+    public class ChoiceNode : DialogNode
+    {
+        public string text;
+
+        [TableList]
+        public List<Choice> choices;
+
+        public class Choice
+        {
+            public string text;
+            public string branch;
+        }
+    }
+
+    public class DialogBranch
+    {
+        public string id;
+        public List<DialogNode> nodes = new List<DialogNode>();
+    }
+    
+    [CreateAssetMenu(
+        fileName = "Dialog",
+        menuName = "[Game]/Configs/Dialog",
+        order = 2)]
+    public class Dialog : ScriptableAsset
+    {
+        public List<DialogBranch> branches;
+    }
+}
--- Assets/_XandArt/TheGrowth/Code/Configs/GameStep.cs
+++ Assets/_XandArt/TheGrowth/Code/Configs/GameStep.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Sirenix.OdinInspector;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using XandArt.Architecture;
 
