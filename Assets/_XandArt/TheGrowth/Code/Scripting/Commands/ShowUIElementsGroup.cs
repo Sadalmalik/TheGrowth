e2c39a9310774e1f12482820b01506bd0ac8ac9f
Gleb Kuzmin <kuzmin@owlcat.games>
2025-09-02 17:54:18 +0200

    Фикс GUID-ов, открытие-закрытие окна инвентаря на экране экспедиции

---
+++ Assets/_XandArt/TheGrowth/Code/Scripting/Commands/ShowUIElementsGroup.cs
@@ -0,0 +1,19 @@
+﻿namespace XandArt.TheGrowth
+{
+    public class ShowUIElementsGroup : Command
+    {
+        public string GroupID;
+        public GroupMode Mode;
+
+        public override void Execute(Context context)
+        {
+            CanvasElementsGroup.SetGroupActive(GroupID, Mode == GroupMode.Show);
+        }
+
+        public enum GroupMode
+        {
+            Show,
+            Hide
+        }
+    }
+}
---
+++ Assets/_XandArt/TheGrowth/Code/UI/Common/CanvasElementsGroup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XandArt.TheGrowth
+{
+    public class CanvasElementsGroup : MonoBehaviour
+    {
+        private static Dictionary<string, HashSet<CanvasElementsGroup>> m_Groups;
+
+        public static HashSet<CanvasElementsGroup> GetGroup(string groupId)
+        {
+            return m_Groups.TryGetValue(groupId, out var group) ? group : null;
+        }
+
+        public static void SetGroupActive(string groupId, bool active)
+        {
+            var group = GetGroup(groupId);
+            if (group == null) return;
+            foreach (var element in group)
+                element.gameObject.SetActive(active);
+        }
+
+        [SerializeField]
+        private string m_GroupId;
+
+        [SerializeField]
+        private bool m_HideOnAwake;
+
+        private void Awake()
+        {
+            m_Groups ??= new Dictionary<string, HashSet<CanvasElementsGroup>>();
+            if (!m_Groups.TryGetValue(m_GroupId, out var groups))
+                m_Groups[m_GroupId] = groups = new HashSet<CanvasElementsGroup>();
+
+            groups.Add(this);
+
+            if (m_HideOnAwake)
+                gameObject.SetActive(false);
+        }
+    }
+}
