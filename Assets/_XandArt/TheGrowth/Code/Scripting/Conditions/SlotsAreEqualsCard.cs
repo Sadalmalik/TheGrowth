3e71eccbc5db124f7b48050bc602f5b9f48bdc8a
Gleb Kuzmin <kuzmin@owlcat.games>
2025-10-25 19:20:31 +0200

    Новый способ смерти

---
+++ Assets/_XandArt/TheGrowth/Code/Scripting/Conditions/SlotsAreEqualsCard.cs
@@ -0,0 +1,19 @@
+﻿namespace XandArt.TheGrowth
+{
+    /// <summary>
+    /// Проверяет, если два слота совподают
+    /// </summary>
+    public class SlotsAreEqualsCard : Condition
+    {
+        public Evaluator<SlotEntity> SlotA = new SlotFromCard { Card=new ActiveCard() };
+        public Evaluator<SlotEntity> SlotB = new BoardSlot();
+
+        public override bool Check(Context context)
+        {
+            var slotA = SlotA.Evaluate(context);
+            var slotB = SlotB.Evaluate(context);
+
+            return slotA == slotB;
+        }
+    }
+}
