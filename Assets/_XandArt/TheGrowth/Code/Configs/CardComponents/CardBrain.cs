72fe71f7d9e4a68d03b9753f2156f5ee20b5b181
Gleb Kuzmin <kuzmin@owlcat.games>
2026-01-19 18:33:30 +0100

    Fix game step

--- Assets/_XandArt/TheGrowth/Code/Configs/CardComponents/CardBrain.cs
+++ Assets/_XandArt/TheGrowth/Code/Configs/CardComponents/CardBrain.cs
@@ -24,7 +24,7 @@ namespace XandArt.TheGrowth
         public CardType Type;
         public bool CanBeDragged;
         public float StepDuration;
-        
+
         [Space]
         public Evaluator<SlotEntity> SpawnSlot;
 
@@ -32,6 +32,7 @@ namespace XandArt.TheGrowth
 
         [Space]
         public List<Command> OnPlacedFirstTime;
+
         public List<Command> OnPlaced;
         public List<Command> OnFlipped;
         public List<Command> OnStep;
@@ -51,7 +52,7 @@ namespace XandArt.TheGrowth
 
             [JsonProperty]
             public Vector3 Position;
-            
+
             [JsonProperty]
             public bool IsFaceUp;
 
@@ -64,13 +65,13 @@ namespace XandArt.TheGrowth
                 get => _slot;
                 set => _slot = value;
             }
-            
+
             [JsonIgnore]
             public CardType Type => Settings.Type;
-            
+
             [JsonIgnore]
             public bool CanBeDragged => Settings.CanBeDragged;
-            
+
             [JsonIgnore]
             public float StepDuration => Settings.StepDuration;
 
@@ -84,6 +85,7 @@ namespace XandArt.TheGrowth
                     onComplete?.Invoke();
                     return;
                 }
+
                 if (view.IsFaceUp != IsFaceUp)
                     view.Flip(onComplete, instant);
                 onComplete?.Invoke();
@@ -97,54 +99,100 @@ namespace XandArt.TheGrowth
 
             public void OnPlacedFirstTime()
             {
-                var context = new Context(
-                    Game.BaseContext,
-                    new ActiveCard.Data { Card = Owner });
-                Settings.OnPlacedFirstTime.ExecuteAll(context);
+                try
+                {
+                    var context = new Context(
+                        Game.BaseContext,
+                        new ActiveCard.Data { Card = Owner });
+                    Settings.OnPlacedFirstTime.ExecuteAll(context);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
             }
 
             public void OnPlaced()
             {
-                var context = new Context(
-                    Game.BaseContext,
-                    new ActiveCard.Data { Card = Owner });
-                Settings.OnPlaced.ExecuteAll(context);
+                try
+                {
+                    var context = new Context(
+                        Game.BaseContext,
+                        new ActiveCard.Data { Card = Owner });
+                    Settings.OnPlaced.ExecuteAll(context);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
             }
 
             public void OnFlipped()
             {
-                var context = new Context(
-                    Game.BaseContext,
-                    new ActiveCard.Data { Card = Owner });
-                Settings.OnFlipped.ExecuteAll(context);
+                try
+                {
+                    var context = new Context(
+                        Game.BaseContext,
+                        new ActiveCard.Data { Card = Owner });
+                    Settings.OnFlipped.ExecuteAll(context);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
             }
 
             public void OnCovered(CompositeEntity coverCard)
             {
-                var context = new Context(
-                    Game.BaseContext,
-                    new ActiveCard.Data { Card = Owner },
-                    new CoveringCard.Data { Card = coverCard });
-                Settings.OnCovered.ExecuteAll(context);
+                try
+                {
+                    var context = new Context(
+                        Game.BaseContext,
+                        new ActiveCard.Data { Card = Owner },
+                        new CoveringCard.Data { Card = coverCard });
+                    Settings.OnCovered.ExecuteAll(context);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
             }
 
             public void OnUnCovered(CompositeEntity coverCard)
             {
-                var context = new Context(
-                    Game.BaseContext,
-                    new ActiveCard.Data { Card = Owner },
-                    new CoveringCard.Data { Card = coverCard });
-                Settings.OnUnCovered.ExecuteAll(context);
+                try
+                {
+                    var context = new Context(
+                        Game.BaseContext,
+                        new ActiveCard.Data { Card = Owner },
+                        new CoveringCard.Data { Card = coverCard });
+                    Settings.OnUnCovered.ExecuteAll(context);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
             }
 
             public bool OnStep()
             {
-                if (!IsFaceUp)
-                    return false;
-                var newContext = new Context(
-                    Game.BaseContext,
-                    new ActiveCard.Data { Card = Owner });
-                return Settings.OnStep.ExecuteAll(newContext);
+                try
+                {
+                    if (!IsFaceUp)
+                        return false;
+                    if (Settings.OnStep == null || Settings.OnStep.Count == 0)
+                        return false;
+                    var newContext = new Context(
+                        Game.BaseContext,
+                        new ActiveCard.Data { Card = Owner });
+                    return Settings.OnStep.ExecuteAll(newContext);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
+
+                return false;
             }
 
             public HashSet<SlotEntity> GetAllowedMoves()
