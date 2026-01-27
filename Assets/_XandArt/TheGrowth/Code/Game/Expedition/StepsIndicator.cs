1e66786cb29b2e242e0d2432f62cc6522de444d7
Gleb Kuzmin <kuzmin@owlcat.games>
2026-01-27 18:39:33 +0100

    Фикс индикатора

--- Assets/_XandArt/TheGrowth/Code/Game/Expedition/StepsIndicator.cs
+++ Assets/_XandArt/TheGrowth/Code/Game/Expedition/StepsIndicator.cs
@@ -1,6 +1,8 @@
 ﻿using TMPro;
 using UnityEngine;
+using XandArt.Architecture.Events;
 using XandArt.Architecture.IOC;
+using XandArt.TheGrowth.StoryLine;
 
 namespace XandArt.TheGrowth
 {
@@ -15,11 +17,22 @@ namespace XandArt.TheGrowth
         public override void Init()
         {
             _manager.OnStepUpdate += UpdateSteps;
+            EventBus.Global.Subscribe<ExpeditionStartEvent>(OnEvent);
+        }
+        
+        private void OnDestroy()
+        {
+            EventBus.Global.Unsubscribe<ExpeditionStartEvent>(OnEvent);
         }
         
         private void UpdateSteps()
         {
             label.SetText(_manager.Steps.ToString());
+        }
+
+        private void OnEvent(ExpeditionStartEvent @event)
+        {
+            UpdateSteps();
         }
     }
 }
